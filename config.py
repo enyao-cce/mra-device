1ef20f7ece6819f677666549c8e8a19fccc46720
enyao-cce <enyao@crystalclear.com.sg>
2024-05-16 11:54:32 +0800

    added config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+BROKER_ADDRESS = 'mqtt.example.com'
+CA_CERT= 'path/to/ca_cert'
+CLIENT_CERT = 'path/to/client_cert'
+CLIENT_KEY = 'path/to/client_key'
+TOPIC = 'topic/subtopic'
+
+MONTHLY_CACHE = 'path/to/monthly_cache.json'
+OFFLINE_CACHE = 'path/to/offline_cache.json'
